c055cd123cef30717dc1af5a46d3ab241979a798
Ryan Stanley <stanleyrya@gmail.com>
2015-10-24 05:35:07 -0400

    addressed merge conflicts, added authorization (client_credentials) and getTrackList

--- spotify.js
+++ spotify.js
@@ -1,29 +1,84 @@
 var exports = module.exports = {};
 var https = require('https');
 
-var defaultOptions = {
-    host : 'api.spotify.com',
-    port : 443,
-    path : '/v1'
-};
+function encode(string){
+    return new Buffer(string).toString('base64');
+}
+
+function authorize(callback){
+    var body = 'grant_type=client_credentials';
+    var headers = {
+        'Authorization' : 'Basic ' + encode(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET),
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Content-Length' : Buffer.byteLength(body, 'utf8')
+    };
+    var options = {
+        host : 'accounts.spotify.com',
+        method: 'POST',
+        path : '/api/token',
+        headers : headers
+    };
+    makeRequest(options, function(data){
+        var access_token = data.access_token;
+        callback(access_token);
+    }, body);
+}
+
+// given a JSON playlist = { ownerID: "...", playlistID: "..." }
+exports.getTrackList = function(playlist, callback){
+    authorize(function(access_token){
+        var headers = {
+            'Authorization' : 'Bearer ' + access_token,
+            'Content-Type' : 'application/json'
+        };
+        var options = {
+            host : 'api.spotify.com',
+            method : 'GET',
+            path : '/v1/users/' + playlist.ownerID + '/playlists/' + playlist.playlistID + '/tracks?market=ES',
+            headers : headers
+        };
+        makeRequest(options, function(data){
+            console.log(data);
+            callback(data);
+        });
+    });
+}
 
 exports.getPlaylist = function(word, callback){
-    //clone default options
-    var options = JSON.parse(JSON.stringify(defaultOptions));
-    options.method = 'GET';
-    options.path += '/search?type=playlist&limit=1&q=';
-    options.path += word;
-    makeRequest(options, callback);
+    var options = {
+        host : 'api.spotify.com',
+        method : 'GET',
+        path : '/v1//search?type=playlist&limit=1&q=' + word,
+        headers : headers
+    };
+    makeRequest(options, function(data){
+        console.log(data);
+        callback(data);
+    });
 };
 
 // given an options JSON, make a request
-function makeRequest(options, callback) {
-    var response;
+function makeRequest(options, callback, reqBody) {
     var req = https.request(options, function(res) {
-        res.on('data', function(d) {
-            callback(d); // execute call back to serve the data as the response
+        var resBody = '';
+
+        // add each data chunk
+        res.on('data', function(data) {
+            data = data.toString("utf-8"); // read the Buffer
+            resBody += data
+        });
+
+        res.on('end', function() {
+            resBody = JSON.parse(resBody);
+            callback(resBody);
         });
     });
+
+    // send request body if it exists
+    if(reqBody !== undefined){
+        req.write(reqBody);
+    }
+
     req.end();
     req.on('error', function(e) {
         console.error(e);
