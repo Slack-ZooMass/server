856561af9e84ba7baf8fec9116e040f05efe5fa9
Ryan Stanley <stanleyrya@gmail.com>
2015-11-25 22:44:12 -0500

    added first draft of pebble support

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var build_playlist = require('./routes/build-playlist');
+var pebble = require('./routes/pebble.js');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/build-playlist', build_playlist);
+app.use('/pebble', pebble);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/pebble.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+var querystring = require('querystring');
+var spotify = require('../lib/spotify');
+var api = require('../lib/api');
+
+// either login or query
+router.get('/', function(req, res, next) {
+  res.render('pebble-index', { title: data });
+});
+
+// authenticate with spotify
+router.get('/login', function(req, res, next) {
+
+  var hostname = req.headers.host; // hostname = 'localhost:8080'
+  var pathname = 'http://'+hostname+'/pebble/callback'; // pathname = '/MyApp'
+
+  var query = {
+    client_id : process.env.CLIENT_ID,
+    response_type : 'code',
+    redirect_uri : pathname,
+    scope : 'playlist-modify-public'
+  }
+
+  res.redirect('https://accounts.spotify.com/authorize?' + querystring.stringify(query));
+});
+
+// get authentication token and save to cookie
+router.get('/callback', function(req, res, next) {
+  var error = req.query.error
+  if(error) {
+    res.render('index', { title : error });
+  }
+
+  var hostname = req.headers.host; // hostname = 'localhost:8080'
+  var pathname = 'http://'+hostname+'/pebble/callback'; // pathname = '/MyApp'
+
+  var code = req.query.code;
+  var authenticationInformation = {grant_type: 'authorization_code', code: code, redirect_uri: pathname};
+  spotify.requestToken(authenticationInformation, function(data) {
+
+    var config_settings = {
+      access_token : data.access_token,
+      refresh_token : data.refresh_token
+    }
+
+    // set the return URL depending on the runtime environment
+    var return_to = getQueryParam('return_to', 'pebblejs://close#');
+    res.redirect(return_to + encodeURIComponent(JSON.stringify(config_settings)));
+  });
+});
+
+module.exports = router;
